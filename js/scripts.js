ca6df48b5cd1b95c7ad23637f6d91d81671d8414
abuyusr <abdizm@outlook.com>
2022-12-27 22:33:06 -0500

    if applied, other condition is added.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,9 @@ if (age === 20) {
   } else {
     console.log('You are not 20 years old.');
   }
+
+  if (pokemon.size > 1.5) {
+    console.log ("This is a big pokomen!!")
+  } else if (pokemon.size > 0.5 && pokemon.size < 1.5){
+    console.log ("This is an average pokemon");
+  } else  { console.log ("This is a small pokemon")}
