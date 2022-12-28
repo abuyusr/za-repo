1c1ad0d444f137ced856e372efd98b287074f0e2
abuyusr <abdizm@outlook.com>
2022-12-27 22:36:33 -0500

    if applied, will update the condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 alert('Hello family');
 // added favorite food
 
-if (age === 20) {
-    console.log('You are 20 years old!');
-  } else {
-    console.log('You are not 20 years old.');
-  }
-
+  let pokemon = {name:"Eve" , size:1.2};
+  
   if (pokemon.size > 1.5) {
     console.log ("This is a big pokomen!!")
   } else if (pokemon.size > 0.5 && pokemon.size < 1.5){
