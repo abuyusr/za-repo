47f5ac00584307fb9c9c54400f44325e94e8d1d2
abuyusr <abdizm@outlook.com>
2022-12-27 22:32:22 -0500

    if applied , new conditions is added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 alert('Hello family');
 // added favorite food
 
-if (age===20) {
-    console.log('You are 20 years!');
-}
+if (age === 20) {
+    console.log('You are 20 years old!');
+  } else {
+    console.log('You are not 20 years old.');
+  }
