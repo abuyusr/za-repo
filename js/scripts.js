7c9eb00245deb343bc28c5bc8bb73e9efea11c58
abuyusr <abdizm@outlook.com>
2022-12-28 22:27:48 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 alert('Hello family');
 // added favorite food
 
-  let pokemon = {name:"Eve" , size:1.2};
-  
-  if (pokemon.size > 1.5) {
-    console.log ("This is a big pokomen!!")
-  } else if (pokemon.size > 0.5 && pokemon.size < 1.5){
-    console.log ("This is an average pokemon");
-  } else  { console.log ("This is a small pokemon")}
+let pokemonList =[]
+
+
+
+
+
+for (let i=0; i< pokemonList.length;i++) {
+  if (pokemonList[i].height >= 7) {
+      document.write (pokemonList[i].name + "(height:" + pokemonList[i].height + "m) - Wow, that is big!" + "<br>")
+  } else if (pokemonList[i].height >= 4 && pokemonList[i].height < 6) {
+      document.write (pokemonList[i].name + "(height:" + pokemonList[i].height + " m ) - Wow, that is big!" + "<br>")
+  } else {
+      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + " m ) - This is small Pokemon "+" <br>")
+  }
+}
+
+
+
