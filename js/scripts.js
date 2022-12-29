ddca8c681370128b36040e156fa142afdf98ce68
abuyusr <abdizm@outlook.com>
2022-12-28 22:31:41 -0500

    if applied , this will updates second pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,11 @@ let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
 
 for (let i=0; i< pokemonList.length;i++) {
   if (pokemonList[i].height >= 7) {
-      document.write (pokemonList[i].name + "(height:" + pokemonList[i].height + "m) - Wow, that is big!" + "<br>")
+      document.write (pokemonList[i].name + " (height:" + pokemonList[i].height + "m) - Wow, that is big!" + "<br>")
   } else if (pokemonList[i].height >= 4 && pokemonList[i].height < 6) {
-      document.write (pokemonList[i].name + "(height:" + pokemonList[i].height + " m ) - Wow, that is big!" + "<br>")
+      document.write (pokemonList[i].name + " (height:" + pokemonList[i].height + " m ) - This is medium" + "<br>")
   } else {
-      document.write(pokemonList[i].name + "(height:" + pokemonList[i].height + " m ) - This is small Pokemon "+" <br>")
+      document.write(pokemonList[i].name + " (height:" + pokemonList[i].height + " m ) - This is small Pokemon "+" <br>")
   }
 }
 
