ab3650fdbd3e8782f79c110f59dee38174974704
abuyusr <abdizm@outlook.com>
2022-12-28 22:28:35 -0500

    if applied , this will add new loop .

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-alert('Hello family');
-// added favorite food
 
-let pokemonList =[]
-
-
-
+alert('Hello world');
+// added new pokomen variables
+let pokemonList =[ {name:'Bulbasaur', type:'grass', height:3 },
+                   {name:'Ivysaur', type:'poison', height:7 },
+                   {name:'Blastoise', type:'water', height:5 }    
+                   ]
+ // Used document.write() inside the loop’s code
 
 
 for (let i=0; i< pokemonList.length;i++) {
